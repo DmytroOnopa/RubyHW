46c2c116bf567694a40a1ed0da9ab5f06b07eeea
theGrove <51022274+DmytroOnopa@users.noreply.github.com>
2021-12-03 22:41:43 +0200

    update

--- HW3/getHtml/lib/getHtml.rb
+++ HW3/getHtml/lib/getHtml.rb
@@ -1,4 +1,26 @@
-def getHtml
-  importedContend = gets.chomp
-  File.open('../index.html', 'w')
+class GetHtml
+
+  def get(file_name = 'index.html')
+    inputContent = gets.chomp
+    inputContent = file_name if inputContent == ''
+    new_html = <<HTML
+    
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>no title</title>
+  </head>
+  <body>
+    <p>#{inputContent}</p>
+ </body>
+</html>
+HTML
+
+    File.open('index.html', 'w') do |f|
+      f.write(string "#{new_html}")
+    end
+  end
 end
+
+GetHtml.new.get
