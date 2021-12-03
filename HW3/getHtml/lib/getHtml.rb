74e68599cc2fe086b1c0b94515900aed525dadf0
theGrove <51022274+DmytroOnopa@users.noreply.github.com>
2021-12-03 23:06:19 +0200

    get Html

--- HW3/getHtml/lib/getHtml.rb
+++ HW3/getHtml/lib/getHtml.rb
@@ -1,10 +1,9 @@
 class GetHtml
-
   def get(file_name = 'index.html')
     inputContent = gets.chomp
     inputContent = file_name if inputContent == ''
     new_html = <<HTML
-    
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -18,7 +17,7 @@ class GetHtml
 HTML
 
     File.open('index.html', 'w') do |f|
-      f.write(string "#{new_html}")
+      f.write("#{new_html}")
     end
   end
 end
