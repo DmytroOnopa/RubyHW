9431c225861310d6c3ff280a99a1c2b36508f41c
theGrove <51022274+DmytroOnopa@users.noreply.github.com>
2021-12-04 21:01:18 +0200

    regenerated

--- HW3/getHtml/lib/getHtml.rb
+++ HW3/getHtml/lib/getHtml.rb
@@ -1,15 +1,14 @@
 class GetHtml
   def get(content, bypass_html, file_name = 'index.html')
-#    markup = gets.chomp
     markup = content.gsub!(/[<>]/, '') if bypass_html == false
     markup = content unless bypass_html == false
-
+    markup = gets.chomp
     
     f = File.new("#{Dir.pwd}/#{file_name}", "w+")
     f.puts "<!DOCTYPE html>"
     f.puts "  <head>"
     f.puts "    <meta charset='utf-8'>"
-    f.puts "    <title>My Tamago</title>"
+    f.puts "    <title>Generated in getHtml</title>"
     f.puts "  </head>"
     f.puts "  <body>"
     f.puts "    <script>"
