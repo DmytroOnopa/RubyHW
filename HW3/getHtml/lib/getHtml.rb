179e3e90f7bfaee9528b4a7e42fb2cdcb0cafdbb
DmytroOnopa <dmytro.onopa@gmail.com>
2021-12-04 17:53:22 +0200

    change HTML making method

--- HW3/getHtml/lib/getHtml.rb
+++ HW3/getHtml/lib/getHtml.rb
@@ -2,23 +2,22 @@ class GetHtml
   def get(file_name = 'index.html')
     inputContent = gets.chomp
     inputContent = file_name if inputContent == ''
-    new_html = <<HTML
+    
+    f = File.new("#{Dir.pwd}/#{file_name}", "w+")
+    f.puts "<!DOCTYPE html>"
+    f.puts "  <head>"
+    f.puts "    <meta charset='utf-8'>"
+    f.puts "    <title>My Tamago</title>"
+    f.puts "  </head>"
+    f.puts "  <body>"
+    f.puts "    <script>"
+    f.puts "      setInterval(()=>{ window.location.reload() }, 2000)"
+    f.puts "    </script>"
+    f.puts "    #{inputContent}"
+    f.puts "  </body>"
+    f.puts "</html>"
+    f.close
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>no title</title>
-  </head>
-  <body>
-    <p>#{inputContent}</p>
- </body>
-</html>
-HTML
-
-    File.open('index.html', 'w') do |f|
-      f.write("#{new_html}")
-    end
   end
 end
 
