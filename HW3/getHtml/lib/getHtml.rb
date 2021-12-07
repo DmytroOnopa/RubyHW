e55e20c29dee8aac5a3c1cee301665f5ca067ee0
theGrove <51022274+DmytroOnopa@users.noreply.github.com>
2021-12-07 20:23:05 +0200

    on leason

--- HW3/getHtml/lib/getHtml.rb
+++ HW3/getHtml/lib/getHtml.rb
@@ -1,8 +1,8 @@
 class GetHtml
-  def get(content, bypass_html, file_name = 'index.html')
-    markup = content.gsub!(/[<>]/, '') if bypass_html == false
-    markup = content unless bypass_html == false
+  def get(content, bypass_html = true, file_name = 'index.html')
     markup = gets.chomp
+    markup = content.gsub!(/[<>]/, '') if bypass_html
+#    markup = content if bypass_html
     
     f = File.new("#{Dir.pwd}/#{file_name}", "w+")
     f.puts "<!DOCTYPE HTML>"
