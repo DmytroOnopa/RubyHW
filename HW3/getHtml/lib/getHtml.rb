723c74fddfd400e7570749df37027d5b26313281
DmytroOnopa <dmytro.onopa@gmail.com>
2021-12-13 00:34:58 +0200

    after rubocop

--- HW3/getHtml/lib/getHtml.rb
+++ HW3/getHtml/lib/getHtml.rb
@@ -2,22 +2,21 @@ class GetHtml
   def get(content, bypass_html = true, file_name = 'index.html')
     markup = gets.chomp
     markup = content.gsub!(/[<>]/, '') if bypass_html
-#    markup = content if bypass_html
-    
-    f = File.new("#{Dir.pwd}/#{file_name}", "w+")
-    f.puts "<!DOCTYPE HTML>"
-    f.puts '<html lang="en">'
-    f.puts "  <head>"
-    f.puts '    <meta charset="UTF-8">'
-    f.puts "    <title>Generated in getHtml</title>"
-    f.puts "  </head>"
-    f.puts "  <body>"
-    f.puts "    <p>#{markup}</p>"
-    f.puts "  </body>"
-    f.puts "</html>"
-    f.close
+    #    markup = content if bypass_html
 
+    f = File.new("#{Dir.pwd}/#{file_name}", 'w+')
+    f.puts '<!DOCTYPE HTML>'
+    f.puts '<html lang="en">'
+    f.puts '  <head>'
+    f.puts '    <meta charset="UTF-8">'
+    f.puts '    <title>Generated in getHtml</title>'
+    f.puts '  </head>'
+    f.puts '  <body>'
+    f.puts "    <p>#{markup}</p>"
+    f.puts '  </body>'
+    f.puts '</html>'
+    f.close
   end
 end
 
-GetHtml.new.get("markup", true)
+GetHtml.new.get('markup', true)
